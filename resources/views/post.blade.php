6fefaad80f9d05b6d5add19a089eb9ec1ce53173
n.rezaei <nourollahr@gmail.com>
2021-10-13 10:24:01 +0330

    create post with link to single post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>
+            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>
+            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
+        </p>
+    </article>
+</body>
