ed7bd369299672be67b2afebb1022e97648c613c
n.rezaei <nourollahr@gmail.com>
2021-10-14 18:30:51 +0330

    store blog posts as html files
    video 8 number: store blog posts as html files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,12 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/post">My First Post</a></h1>
-        <p>
-            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
-        </p>
-    </article>
+<article>
+    <?= $post; ?>
+</article>
 
-    <a href="/">Go Back</a>
+<a href="/">Go Back</a>
 </body>
