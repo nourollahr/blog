a41d8827d8a4374b70077d22274b524be571b415
n.rezaei <nourollahr@gmail.com>
2021-10-17 06:46:12 +0330

    read all posts with the FileSystem class
    video 11: read all posts with the FileSystem class

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,24 +4,9 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        <p>
-            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>
-            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
-        </p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>
-            The spirit has sainthood, but not everyone needs it.Never believe the monkey, for you cannot invent it.
-        </p>
-    </article>
+    @foreach($posts as $post)
+        <article>
+            {!! $post !!}
+        </article>
+    @endforeach
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,16 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts' ,[
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-    if (! file_exists($path)) {
-        abort(404);
-    }
+    return view('post',[
+        'post' => Post::find($slug)
+    ]);
 
-    $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-    return view('post', ['post' => $post]);
 })->where('post', '[a-z_\-]+');
